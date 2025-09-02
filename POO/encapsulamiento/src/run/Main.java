5e7e4bd241d39412856c59eafc033af270e34caa
Garu006 <garojas@uamv.edu.ni>
2025-09-01 18:29:50 -0600

    clase de poo 9/1/2025

---
+++ POO/encapsulamiento/src/run/Main.java
@@ -0,0 +1,14 @@
+package run;
+
+import models.Course;
+import models.Teacher;
+
+import java.util.zip.CheckedOutputStream;
+
+public class Main {
+    public static void main(String[] args){
+        Teacher teacher = new Teacher("Gabriel Rojas");
+        Course course = new Course("Java programming", teacher);
+
+    }
+}
